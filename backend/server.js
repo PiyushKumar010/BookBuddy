87c15dc9c6c435c4be0a3ae2bd679639d6a592d7
Piyush Kumar <piyush31221@gmail.com>
2025-09-03 13:06:52 +0530

    Update server.js
    fix the bug

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,13 @@ import cors from 'cors';
 
 // Load environment variables from .env file
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: "https://bookbuddy-iota.vercel.app",
   credentials: true,
 }));
-
-const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
